81417d1688651adc6d369ec8128a6f4120f241c8
karolnedza <37583943+karolnedza@users.noreply.github.com>
2021-09-14 14:53:34 +0200

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aviatrix = {
       source = "aviatrixsystems/aviatrix"
-      version = "2.19.4"
+      version = "2.20"
     }
   }
   required_version = ">= 0.15"
